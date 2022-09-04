bc1045bc921fbda3476a26551bbb26553d21cadc
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-09-04 22:33:30 +0500

    Задача

---
+++ 002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача
+
+string[] A;
+System.Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+A = stroka.Split(' ');
+var result = new string[A .Length];
+var realSize = 0;
+foreach (var value in A )
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+System.Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+Console.ReadKey(true);
