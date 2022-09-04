a0c093016b643e7b7d465a29e5d40ce46edd95ef
pavelyakovenko2012 <pavel.yackovencko2012@gmail.com>
2022-09-04 22:25:36 +0500

    Задача про Машу

---
+++ 001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Решение примера про Машу
+
+System.Console.Write("Введите ваше имя:");
+string username = Console.ReadLine()!;
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Я так ждала тебя, Маша");
+}
+else
+{
+    System.Console.Write("Привет");
+    System.Console.WriteLine(username);
+}
